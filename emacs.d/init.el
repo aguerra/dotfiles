a4a58cb08a0f775f533eb77ec29765715321f67c
Alex Guerra <alex9627@gmail.com>
2018-10-14 20:15:57 -0300

    [emacs] Remove auto cleaning of whitespace

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -45,7 +45,6 @@
   :init
   (dolist (hook '(prog-mode-hook text-mode-hook))
     (add-hook hook #'whitespace-mode))
-  (add-hook 'before-save-hook #'whitespace-cleanup)
   :config
   (setq whitespace-line-column 80)
   (setq whitespace-style '(face tabs empty trailing lines)))
