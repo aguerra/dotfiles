db765d3a280e23e2c00914f825fb4228cd420d1e
Alex Guerra <alex9627@gmail.com>
2019-05-28 15:54:17 -0300

    [emacs] Remove zop-to-char package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -280,12 +280,6 @@
 (use-package yaml-mode
   :ensure t)
 
-(use-package zop-to-char
-  :ensure t
-  :bind
-  (("M-z" . zop-up-to-char)
-   ("M-Z" . zop-to-char)))
-
 ;; Custom functions
 (defun cider-show-repl-buffer ()
   (interactive)
